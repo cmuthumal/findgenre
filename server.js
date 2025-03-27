321efd5d7839f42de91480bc52021e6bda2aba73
Chanaka Gunasekara <chanaka.gunasekara@thegpservice.co.uk>
2025-03-28 01:26:15 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + '/dist/findgenre'));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/findgenre/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log("Server started");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
